16853e9cebd1e75531030cccb1a545473ee45365
Rauã Hipólito <rauavasconcelos22@gmail.com>
2024-10-16 21:27:27 -0300

    Instalando react router dom e criando páginas com roteamento no App.tsx

---
+++ src/pages/AddTodo.tsx
@@ -0,0 +1,9 @@
+function AddTodo() {
+    return(
+        <>
+        <h1>Página de AddTodo</h1>
+        </>
+    )
+}
+
+export default AddTodo;
---
+++ src/pages/TodoList.tsx
@@ -0,0 +1,9 @@
+function TodoList() {
+    return(
+        <>
+        <h1>Página de TodoList</h1>
+        </>
+    )
+}
+
+export default TodoList;
