4b72427723b6e21d9e5274339118c4dbccf2ed9c
Rauã Hipólito <rauavasconcelos22@gmail.com>
2024-10-22 16:14:47 -0300

    ⚡️ Criando Estado e hooks para gerenciar formulário com nome e senha

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,39 @@
+import { useState } from 'react';
 import Banner from '../assets/banner.svg';
 
 function Login() {
+
+    const [input, setInput] = useState({
+        username: '',
+        password: ''
+    });
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        
+        setInput({
+            ...input,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    function handleSubmit(event: React.FormEvent) {
+      event.preventDefault();
+     console.log(input);
+     
+    };
+
     return(
         <>
         <h1>Welcome</h1>
         <img src={ Banner } alt="Banner da tela de login" />
-        <input type="text" placeholder='Enter your username' />
-        <input type="password" placeholder='Enter your passaword' />
+
+        <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} id='username' name='username' type="username"placeholder='Enter your username' />
+        <input onChange={handleChange} type="password" id='password' name='password' placeholder='Enter your passaword' />
         <p>Forgot Password?</p>
-        <button>Sign in</button>
+        <button>Sign in</button>            
+        </form>
+
         </>
     );
 }
