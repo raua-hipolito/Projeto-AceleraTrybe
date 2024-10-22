53592e968e8ad2ae499a02c6f36f586b85a3372b
Rauã Hipólito <rauavasconcelos22@gmail.com>
2024-10-22 15:39:50 -0300

    Import image page Login and inputs, button and text

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,16 @@
+import Banner from '../assets/banner.svg';
+
 function Login() {
     return(
         <>
-        <h1>Página de Login</h1>
+        <h1>Welcome</h1>
+        <img src={ Banner } alt="Banner da tela de login" />
+        <input type="text" placeholder='Enter your username' />
+        <input type="password" placeholder='Enter your passaword' />
+        <p>Forgot Password?</p>
+        <button>Sign in</button>
         </>
-    )
+    );
 }
 
 export default Login;
